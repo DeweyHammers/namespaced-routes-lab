5bd35667e96e98d9136272235198047d27faf0d5
Dewey <hammersdrichard@gmail.com>
2021-05-12 06:10:25 -0700

    commit tree d67e4f50a92f6109eb2f7d6ffb40150eee2a563d

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,13 +8,17 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if Preference.first.allow_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
     @artist = Artist.new(artist_params)
 
-    if @artist.save
+    if @artist.save 
       redirect_to @artist
     else
       render :new
